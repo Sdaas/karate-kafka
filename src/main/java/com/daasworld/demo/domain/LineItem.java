97c98be54f733f684da7041befd68ef8a20a3f19
Soumendra Daas <soumendra_daas@intuit.com>
2020-02-29 23:03:35 +0530

    Added price and total to LineItem. Modified order to have a list of LineItems.

--- src/main/java/com/daasworld/demo/domain/LineItem.java
+++ src/main/java/com/daasworld/demo/domain/LineItem.java
@@ -3,37 +3,44 @@ package com.daasworld.demo.domain;
 public class LineItem {
     private int id;
     private int quantity;
+    private int price;
+    private int total=0;
 
     public LineItem() {
-
+        // nothing to do here.
     }
 
-    public LineItem(int id, int quantity) {
+    public LineItem(int id, int quantity, int price) {
         this.id = id;
         this.quantity = quantity;
+        this.price = price;
     }
 
     public int getId() {
         return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id) { this.id = id; }
 
     public int getQuantity() {
         return quantity;
     }
-
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
+    public int getPrice() { return price; }
+    public void setPrice(int price) { this.price = price; }
+
+    public int getTotal() { return total;}
+    public void setTotal(int total) { this.total = total; }
 
     @Override
     public String toString() {
         return "LineItem{" +
                 "id=" + id +
                 ", quantity=" + quantity +
+                ", price=" + price +
+                ", total=" + total +
                 '}';
     }
 }
--- src/main/java/com/daasworld/demo/domain/Order.java
+++ src/main/java/com/daasworld/demo/domain/Order.java
@@ -1,54 +1,61 @@
 package com.daasworld.demo.domain;
 
+import java.util.ArrayList;
+
 public class Order {
     private static int nextOrderId = 0;
 
     private int id;
     private Customer customer;
-    private LineItem lineItem;
+    private ArrayList<LineItem> lineItems = new ArrayList<>();
+    private int total=0;
 
     public Order(){
-
-    }
-
-    public Order(Customer c, LineItem lineItem){
-        this.customer = c;
-        this.lineItem = lineItem;
         this.id = nextOrderId;
         nextOrderId++;
     }
 
-    public LineItem getLineItem() {
-        return lineItem;
+    public Order(Customer c){
+        this();
+        setCustomer(c);
     }
 
-    public void setLineItem(LineItem lineItem) {
-        this.lineItem = lineItem;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
 
     public Customer getCustomer() {
         return customer;
     }
-
     public void setCustomer(Customer customer) {
         this.customer = customer;
     }
 
-
-    public int getId() {
-        return id;
+    public ArrayList<LineItem> getLineItems() {
+        // make sure to return a copy
+        ArrayList<LineItem> copy = new ArrayList<>();
+        copy.addAll(lineItems);
+        return copy;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setLineItems(ArrayList<LineItem> lineItems) {
+        this.lineItems = new ArrayList<>();
+        this.lineItems.addAll(lineItems);
     }
+
+    public int getTotal() { return total; }
+    public void setTotal(int total) { this.total = total; }
 
     @Override
     public String toString() {
         return "Order{" +
                 "id=" + id +
                 ", customer=" + customer +
-                ", lineItem=" + lineItem +
+                ", lineItems=" + lineItems +
+                ", total=" + total +
                 '}';
     }
 }
