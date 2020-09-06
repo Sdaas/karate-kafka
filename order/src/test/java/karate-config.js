bc4f4d4c5e70b736fccb2b108468064feaa89b8a
Soumendra Daas <soumendra.daas@gmail.com>
2020-09-06 17:09:20 +0530

    Refactoring this to a single-module project that contains just Karate for Kafka.

--- order/src/test/java/karate-config.js
+++
@@ -1,8 +0,0 @@
-function fn() {
-
-    var config = {
-        baseUrl : 'http://localhost:8080'
-    };
-
-    return config;
-}
--- order/src/test/java/karate/order/DemoRunner.java
+++
@@ -1,8 +0,0 @@
-package karate.order;
-
-import com.intuit.karate.junit4.Karate;
-import org.junit.runner.RunWith;
-
-@RunWith(Karate.class)
-public class DemoRunner {
-}
