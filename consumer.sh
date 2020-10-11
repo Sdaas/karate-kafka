206aa468b72f8b6aca60d761f4a775cbcb963dee
Soumendra Daas <soumendra.daas@gmail.com>
2020-10-11 13:23:44 +0530

    switching to kafkacat

--- consumer.sh
+++ consumer.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+kafkacat -b localhost:9092 -t test-topic -C \
+  -f '\n
+  Key (%K bytes): %k
+  Value (%S bytes): %s
+  Timestamp: %T Partition: %p Offset: %o
+  Headers: %h\n'
 
-kafka-console-consumer \
-    --bootstrap-server localhost:9092 \
-    --topic test-topic \
-    --from-beginning \
-    --formatter kafka.tools.DefaultMessageFormatter \
-    --property print.key=true \
-    --property print.value=true \
-    --property print.timestamp=true \
-    --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
-    --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
+#kafka-console-consumer \
+#    --bootstrap-server localhost:9092 \
+#    --topic test-topic \
+#    --from-beginning \
+#    --formatter kafka.tools.DefaultMessageFormatter \
+#    --property print.key=true \
+#    --property print.value=true \
+#    --property print.timestamp=true \
+#    --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+#    --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
--- startup.sh
+++ startup.sh
@@ -3,7 +3,8 @@
 ## Start up a single single Kafka broker (along with Zookeeper) and create a topic called test-topic
 docker-compose -f kafka-single-broker.yml up -d
 docker ps
-echo "*** sleeping for 10 seconds (give time for containers to spin up)"
+echo "*** sleeping for 15 seconds (give time for containers to spin up)"
 sleep 15
 echo "*** the following topic were created ...."
-kafka-topics -zookeeper 127.0.0.1:2181 --list
+kafkacat -L -b localhost:9092
+#kafka-topics -zookeeper 127.0.0.1:2181 --list
